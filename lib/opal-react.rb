42d18b92f65904fa78b3cb99f313c87cc839e9ac
Luke Barbuto <luke.barbuto@gmail.com>
2015-07-25 22:54:50 -0700

    Disable the prerender data interface

--- lib/opal-react.rb
+++ lib/opal-react.rb
@@ -1,6 +1,6 @@
 if RUBY_ENGINE == 'opal'
   require "opal-react/top_level"
-  require "opal-react/component"  
+  require "opal-react/component"
   require "opal-react/element"
   require "opal-react/event"
   require "opal-react/version"
@@ -10,13 +10,13 @@ if RUBY_ENGINE == 'opal'
   require "opal-react/rendering_context"
   require "opal-react/state"
   require "opal-react/while_loading"
-  require "opal-react/prerender_data_interface"
+  # require "opal-react/prerender_data_interface"
 else
   require "opal"
   require "opal-react/version"
   require "opal-activesupport"
-  require "rails-helpers/react_component"
-  require "opal-react/prerender_data_interface"
+  # require "rails-helpers/react_component"
+  # require "opal-react/prerender_data_interface"
   require "opal-react/serializers"
 
   Opal.append_path File.expand_path('../', __FILE__).untaint
